f06e7845da46d0719de0e88992930abb245b624d
jbcohen4 <144750171+jbcohen4@users.noreply.github.com>
2023-10-16 19:34:04 -0700

    changed comments

--- producer_consumer.c
+++ producer_consumer.c
@@ -8,9 +8,9 @@
 #include <linux/semaphore.h>
 #include <linux/timekeeping.h>
 
-// Module Parameters
+// for some reason I think we need the license?
+// it got mad at us when we didn't have it
 MODULE_LICENSE("GPL");
-// Module License
 static int buffSize;
 static int prod;
 static int cons;
@@ -22,65 +22,52 @@ module_param(cons, int, 0);
 module_param(uuid, int, 0);
 
 
-static struct task_struct *prod_thread = NULL; // Producer thread
+static struct task_struct *prod_thread = NULL;
 static struct task_struct **cons_threads = NULL;
 
-// Semaphores
 struct semaphore mutex;
 struct semaphore empty;
 struct semaphore full;
 
-// Shared Buffer
 static struct task_struct **buffer = NULL;
 int in, out;
 
-// Producer function
-// The code is a function called producer in a C program. This function is part of a producer-consumer pattern,
-// where one or more threads produce data and one or more threads consume data. The producer function is responsible for adding tasks to a shared buffer.
 static int producer(void *data)
 {
    struct task_struct *task;
     
    for_each_process(task)
     {
-	// Attempt to acquire the empty semaphore. If not available or interrupted, break out of the loop.
 	if (down_interruptible(&empty))
 	    break;
 
-	// Attempt to acquire the mutex semaphore. If interrupted, release the empty semaphore and move to next process.
 	if (down_interruptible(&mutex))
 	{
 	    up(&empty);
 	    break;
 	}
 
-	// Critical Section: Add tasks with UID == uuid
 	if (task->cred->uid.val == uuid)
 	{
 	    buffer[in] = task;
 	    printk(KERN_INFO "[Producer] Produced Item at buffer index: %d for PID: %d", in, task->pid);
 	    in = (in + 1) % buffSize;
 
-	    // Signal that the buffer has an item.
+	    // one more slot has been filled
 	    up(&full);
 	}
 	else
 	{
-	    // If task was not added to the buffer, release the empty semaphore.
+	    // If we didn't add anything to the buffer, then we need to undo the down(&empty) that we did earlier
 	    up(&empty);
 	}
-
-	// Release the mutex semaphore.
 	up(&mutex);
     }
     
-    while (!kthread_should_stop())
-    {
-    }
+    while (!kthread_should_stop()){} // we don't want this thread to stop until we signal it to. 
     return 0;
 }
 
-// Consumer function
 static u64 total_elapsed_time = 0;
 
 static int consumer(void *data)
@@ -92,35 +79,28 @@ static int consumer(void *data)
     while (!kthread_should_stop())
     {
         if (down_interruptible(&full))
-            break; // exit the loop if interrupt signal received
+            break;
 
         if (down_interruptible(&mutex))
         {
-            up(&full); // release the full semaphore if interrupted
-            break;     // exit the loop if signal received
+            up(&full);
+            break;
         }
 
-        // Read from the buffer
         task = buffer[out];
-        out = (out + 1) % buffSize; // Circular buffer
+        out = (out + 1) % buffSize;
 
         up(&mutex);
 
-        // Signal that a buffer slot has become empty
         up(&empty);
-        // Update total elapsed time
         elapsed_time = ktime_get_ns() - task->start_time;
         total_elapsed_time += elapsed_time;
-        // Calculate elapsed time
         
-        elapsed_time /= 1000000000; // Convert to seconds
+        elapsed_time /= 1000000000; // convert ns to seconds
         u64 min = (elapsed_time % 3600) / 60;
         u64 hours = elapsed_time / 3600;
         u64 seconds = elapsed_time % 60;
 
-       
-
-        // Log the consumed item and elapsed time
         printk(KERN_INFO "[Consumer-%d] Consumed Item#-%d on buffer index:%d PID:%d Elapsed Time-%02llu:%02llu:%02llu\n",
                (int)data, out, out, task->pid, hours, min, seconds);
     }
@@ -132,7 +112,6 @@ static int __init producer_consumer_init(void)
 {
     printk(KERN_INFO "\nInitializing producer-consumer module\n");
 
-    // Allocate memory for cons_threads based on the cons value
     cons_threads = kmalloc(sizeof(struct task_struct *) * cons, GFP_KERNEL);
     if (!cons_threads)
     {
@@ -140,25 +119,21 @@ static int __init producer_consumer_init(void)
         return -ENOMEM;
     }
 
-    // Allocate memory for buffer based on the buffSize value
     buffer = kmalloc(sizeof(struct task_struct *) * buffSize, GFP_KERNEL);
     if (!buffer)
     {
         printk(KERN_INFO "Error allocating memory for buffer\n");
-        kfree(cons_threads); // Free memory allocated for consumer threads before returning
+        kfree(cons_threads); // free the memory if there was an allocation error
         return -ENOMEM;
     }
 
-    // Initialize buffer index variables
     in = 0;
     out = 0;
 
-    // Initialize Semaphores
-    sema_init(&mutex, 1);        // Mutual exclusion semaphore initialized to 1
-    sema_init(&empty, buffSize); // 'empty' slots initialized to buffSize
-    sema_init(&full, 0);         // 'full' slots initialized to 0
+    sema_init(&mutex, 1);
+    sema_init(&empty, buffSize);
+    sema_init(&full, 0);
 
-    // Create Producer Thread if required
     if (prod == 1)
     {
         prod_thread = kthread_run(producer, NULL, "producer-thread");
@@ -169,7 +144,6 @@ static int __init producer_consumer_init(void)
         }
     }
 
-    // Create Consumer Threads
     for (int i = 0; i < cons; ++i)
     {
         cons_threads[i] = kthread_run(consumer, (void *)(uintptr_t)i, "consumer-thread-%d", i);
@@ -187,23 +161,24 @@ static void __exit producer_consumer_exit(void)
 {
     printk(KERN_INFO "Exiting producer-consumer module\n");
 
-    // Stop all the threads
+    // kill the producer thread
+    // this is why we need to keep the producer thread alive until we send the kill signal (here)
     if (prod_thread && !IS_ERR(prod_thread))
     {
         kthread_stop(prod_thread);
     }
 
+    // kill all the consumer threads
     for (int i = 0; i < cons; i++)
     {
         if (cons_threads[i] && !IS_ERR(cons_threads[i]))
             kthread_stop(cons_threads[i]);
     }
 
-    // Free dynamically allocated memory
+    // free the memory we kmalloc'd
     kfree(cons_threads);
     kfree(buffer);
 
-    // Calculate and print total elapsed time
     total_elapsed_time /= 1000000000;
     u64 min = (total_elapsed_time % 3600) / 60;
     u64 hours = total_elapsed_time / 3600;
