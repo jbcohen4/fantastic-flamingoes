4a29d026f002d1a6cbd67b7a65c1e2234f21bc73
jbcohen4 <144750171+jbcohen4@users.noreply.github.com>
2023-11-08 22:28:37 -0700

    added module license and fixed init and exit fns

--- memory_manager.c
+++ memory_manager.c
@@ -6,16 +6,16 @@
 static int pid;
 module_param(pid, int, 0);
 
+MODULE_LICENSE("GPL");
 
-
-static int __init producer_consumer_init(void)
+static int __init memory_manager_init(void)
 {
     printk(KERN_INFO "\nInitializing memory-manager module\n");
     return 0;
 }
 
 
-static void __exit producer_consumer_exit(void)
+static void __exit memory_manager_exit(void)
 {
     printk(KERN_INFO "Exiting producer-consumer module\n");
 }
