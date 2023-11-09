30bca0b32a1999a1aa4cf609177ba218bf9fa508
jbcohen4 <144750171+jbcohen4@users.noreply.github.com>
2023-11-08 22:47:43 -0700

    added stuff to walk the page table

--- memory_manager.c
+++ memory_manager.c
@@ -16,7 +16,7 @@ static int ptep_test_and_clear_young(struct vm_area_struct *vma, unsigned long a
 {
     int ret = 0;
     if (pte_young(*ptep))
-        ret = test_and_clear_bit(_PAGE_BIT_ACCESSED,
+        ret = test_and_clear_bit(_PAGE_BIT_ACCESSED, (unsigned long *) &ptep->pte);
     return ret; 
 }
 
@@ -32,6 +32,65 @@ static void __exit memory_manager_exit(void)
     printk(KERN_INFO "Exiting producer-consumer module\n");
 }
 
+
+typedef struct {
+    unsigned long rss;
+    unsigned long swap;
+    unsigned long wss;
+} MemoryMeasure;
+
+static MemoryMeasure get_memory_usage(struct task_struct *task) {
+    MemoryMeasure result;
+    mem.rss = 0;
+    mem.swap = 0;
+    mem.wss = 0;
+
+    struct mm_struct *mm = task->mm;
+    struct vm_area_struct *vma = NULL;
+    pgd_t *pgd;
+    p4d_t *p4d;
+    pud_t *pud;
+    pmd_t *pmd;
+    pte_t *pte;
+
+    // Walk through all VMAs of the process
+    for (vma = mm->mmap ; vma; vma = vma->vm_next) {
+        unsigned long address = vma->vm_start;
+
+        // Walk through each page in the VMA
+        for (; address < vma->vm_end; address += PAGE_SIZE) {
+            pgd = pgd_offset(mm, address);
+            if (pgd_none(*pgd) || pgd_bad(*pgd))
+                continue;
+
+            p4d = p4d_offset(pgd, address);
+            if (p4d_none(*p4d) || p4d_bad(*p4d))
+                continue;
+
+            pud = pud_offset(p4d, address);
+            if (pud_none(*pud) || pud_bad(*pud))
+                continue;
+
+            pmd = pmd_offset(pud, address);
+            if (pmd_none(*pmd) || pmd_bad(*pmd))
+                continue;
+
+            pte = pte_offset_map(pmd, address);
+            if (!pte)
+                continue;
+            if (pte_present(*pte)) {
+                result.rss += PAGE_SIZE; // Page is in resident set
+                if (pte_young(*pte) || pte_dirty(*pte)) {
+                    result.wss += PAGE_SIZE; // Page is in working set
+                }
+            } else {
+                result.swap += PAGE_SIZE; // Page is in swap
+            }
+            pte_unmap(pte);
+        }
+    }
+    return result;
+}
 
 
 
