3571e513f6a37453401a7f3de0c16fe06a65fdc5
jbcohen4 <144750171+jbcohen4@users.noreply.github.com>
2023-11-08 21:35:09 -0700

    added module_init and module_exit

--- memory_manager.c
+++ memory_manager.c
@@ -1,1 +1,24 @@
-// nothing!
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/pid.h>
+#include <linux/sched.h>
+
+static int pid;
+module_param(pid, int, 0);
+
+
+
+static int __init producer_consumer_init(void)
+{
+    printk(KERN_INFO "\nInitializing memory-manager module\n");
+    return 0;
+}
+
+
+static void __exit producer_consumer_exit(void)
+{
+    printk(KERN_INFO "Exiting producer-consumer module\n");
+}
+
+module_init(producer_consumer_init);
+module_exit(producer_consumer_exit);
