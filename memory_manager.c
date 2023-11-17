bc0bfdcfc8df8511f8d13b9194169998cd607888
elisemacabou <123762786+elisemacabou@users.noreply.github.com>
2023-11-16 21:16:30 -0700

    Update memory_manager.c

--- memory_manager.c
+++ memory_manager.c
@@ -1,45 +1,45 @@
-#include <linux/module.h>
-#include <linux/kernel.h>
-#include <linux/init.h>
-#include <linux/hrtimer.h>
-#include <linux/ktime.h>
+#include <linux/module.h>       // Needed by all modules
+#include <linux/kernel.h>       // Needed for KERN_INFO
+#include <linux/init.h>         // Needed for the macros
+#include <linux/sched.h>        // Needed for task_struct and current
+#include <linux/mm.h>           // Needed for mm_struct
+#include <linux/mm_types.h>     // Needed for mm_struct
+#include <linux/hrtimer.h>      // Needed for high-resolution timers
+#include <linux/ktime.h>        // Needed for ktime_* functions
+#include <linux/hrtimer.h>      // Needed for the timer callback to work
 
 static int pid;
 module_param(pid, int, 0);
 
 MODULE_LICENSE("GPL");
 
-static struct hrtimer hr_timer;
-static unsigned long timer_interval_ns = 10000000000UL; // 10 seconds in nanoseconds
 
-enum hrtimer_restart timer_callback(struct hrtimer *timer)
+int ptep_test_and_clear_young(struct vm_area_struct *vma, unsigned long addr, pte_t *ptep)
 {
-    ktime_t now, interval;
-
-    now = ktime_get();
-    interval = ktime_set(0, timer_interval_ns);
-    hrtimer_forward(timer, now, interval);
-
-    // Reduce the frequency of logging
-    pr_info_ratelimited("Timer callback\n");
-
-    return HRTIMER_RESTART;
+    int ret = 0;
+    if (pte_young(*ptep))
+        ret = test_and_clear_bit(_PAGE_BIT_ACCESSED, (unsigned long *) &ptep->pte);
+    return ret; 
 }
+
+static struct hrtimer hr_timer; // Declare the high-resolution timer
+static unsigned long timer_interval_ns = 10000000000UL;
 
 static int __init memory_manager_init(void)
 {
-    ktime_t ktime;
+    printk(KERN_INFO "Memory Manager Module loaded with PID: %d\n", pid);
 
-    pr_info("Memory Manager Module loaded with PID: %d\n", pid);
-
-    ktime = ktime_set(0, timer_interval_ns);
+    // Initialize high-resolution timer to fire every 10 seconds
+    ktime_t ktime = ktime_set(0, timer_interval_ns); // 10 seconds
     hrtimer_init(&hr_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
     hrtimer_start(&hr_timer, ktime, HRTIMER_MODE_REL);
     hr_timer.function = timer_callback;
 
-    return 0;
+    return 0; // Return 0 for successful loading
 }
 
+
+// Module cleanup function
 static void __exit memory_manager_exit(void)
 {
     int ret;
@@ -49,6 +49,81 @@ static void __exit memory_manager_exit(void)
         pr_info("The timer was still in use...\n");
 
     pr_info("Memory Manager Module unloaded.\n");
+}
+
+typedef struct {
+    unsigned long rss;
+    unsigned long swap;
+    unsigned long wss;
+} MemoryMeasure;
+
+static MemoryMeasure get_memory_usage(struct task_struct *task) {
+    MemoryMeasure result;
+    result.rss = 0;
+    result.swap = 0;
+    result.wss = 0;
+
+    struct mm_struct *mm = task->mm;
+    struct vm_area_struct *vma = NULL;
+    pgd_t *pgd;
+    p4d_t *p4d;
+    pud_t *pud;
+    pmd_t *pmd;
+    pte_t *pte;
+
+    // Walk through all VMAs of the process
+    for (vma = mm->mmap ; vma; vma = vma->vm_next) {
+        unsigned long address = vma->vm_start;
+
+        // Walk through each page in the VMA
+        for (; address < vma->vm_end; address += PAGE_SIZE) {
+            pgd = pgd_offset(mm, address);
+            if (pgd_none(*pgd) || pgd_bad(*pgd))
+                continue;
+
+            p4d = p4d_offset(pgd, address);
+            if (p4d_none(*p4d) || p4d_bad(*p4d))
+                continue;
+
+            pud = pud_offset(p4d, address);
+            if (pud_none(*pud) || pud_bad(*pud))
+                continue;
+
+            pmd = pmd_offset(pud, address);
+            if (pmd_none(*pmd) || pmd_bad(*pmd))
+                continue;
+
+            pte = pte_offset_map(pmd, address);
+            if (!pte)
+                continue;
+            if (pte_present(*pte)) {
+                result.rss += PAGE_SIZE; // Page is in resident set
+                if (pte_young(*pte) || pte_dirty(*pte)) {
+                    result.wss += PAGE_SIZE; // Page is in working set
+                }
+            } else {
+                result.swap += PAGE_SIZE; // Page is in swap
+            }
+            pte_unmap(pte);
+        }
+    }
+    return result;
+}
+
+
+
+enum hrtimer_restart no_restart_callback(struct hrtimer *timer)
+{
+
+ktime_t now, interval;
+
+    now = ktime_get();
+    interval = ktime_set(0, timer_interval_ns);
+    hrtimer_forward(timer, now, interval);
+	
+    printk(KERN_INFO "It's working!\n");
+
+	return HRTIMER_RESTART;
 }
 
 module_init(memory_manager_init);
